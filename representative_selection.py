650ed0d0d7379ae3a8df7cd44b337a2adf041544
nioushasadjadi <nsadjadi@yahoo.com>
2024-08-01 17:24:08 -0400

    Added multi_dimensional_scaling function and tested it.

--- representative_selection.py
+++ representative_selection.py
@@ -323,7 +323,7 @@ class ChromosomeRepresentativeSelection:
 if __name__ == '__main__':
     human_representative = ChromosomeRepresentativeSelection('human', 6, 'DSSIM')
     for chr_name in human_representative.chromosomes_holder.get_all_chromosomes_name():
-        human_representative.plot_distance_variations(chr_name, plot_random_outliers=False)
+        human_representative.plot_distance_variations(chr_name)
         segments_info = human_representative.get_non_overlapping_segments(chr_name)['segments_information']
         human_representative.plot_multi_dimensional_scaling(human_representative.get_distance_matrix(chr_name),
                                                             segments_info, coloring_type='Chromosome')
